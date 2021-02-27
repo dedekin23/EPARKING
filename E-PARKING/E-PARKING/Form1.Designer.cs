dd2d27feb81b01ef4db7980cd7b5570ec2947d14
MICHAEL B MANULLANG <michaelbonar@365.telkomuniversity.ac.id>
2021-02-27 15:26:42 +0700

    ini message

--- E-PARKING/E-PARKING/Form1.Designer.cs
+++ E-PARKING/E-PARKING/Form1.Designer.cs
@@ -34,10 +34,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Poppins", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Poppins", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(38, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(250, 93);
+            this.label1.Size = new System.Drawing.Size(302, 113);
             this.label1.TabIndex = 0;
             this.label1.Text = "SIGN UP";
             // 
